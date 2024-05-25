f4d9d02d2b34ef380d3e4211a7d37d94b8c3df16
Jhanfer <jhafer73@gmail.com>
2024-05-26 01:19:23 +0200

    GIT Actions

---
+++ remote_build.sh
@@ -0,0 +1,10 @@
+cd link_bio
+source .link_bio\Scripts\activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL="https://api.ukory.es" reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
