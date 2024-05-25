627f15c5462d719ec2fea0873268122b45b78c3a
Jhanfer <jhafer73@gmail.com>
2024-05-25 23:01:13 +0200

    API url

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL="https://api.ukory.es" reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar
+API_URL="https://api.ukory.es" reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://api.ukory.es"
+    api_url="https://api.ukory.es" # se añade la api url crada en rainway 
 )
