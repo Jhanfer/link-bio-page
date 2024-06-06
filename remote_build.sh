9538fdeea43f673708507ef680a47771150d23f3
Jhanfer <jhafer73@gmail.com>
2024-06-07 01:16:04 +0200

    TEST

--- remote_build.sh
+++ remote_build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
+API_URL="https://ukory-web.up.railway.app" reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,12 +2,11 @@ import reflex as rx
 import os
 import dotenv
 
-dotenv.load_dotenv()
+"""dotenv.load_dotenv()
 __API_URL=os.environ.get("API_URL")
-
+"""
 config = rx.Config(
     app_name="link_bio",
-    api_url=__API_URL,
     cors_allowed_origins=[
         "https://www.ukory.es",
         "http://localhost:3000"
