bdd74b18395ba1aeccde6b71fb430af24a6c60c8
Jhanfer <jhafer73@gmail.com>
2024-06-07 12:41:02 +0200

    API TEST

--- remote_build.sh
+++ remote_build.sh
@@ -3,7 +3,11 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL="https://ukory-web.up.railway.app" reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
+API_URL="https://ukory-web.up.railway.app"
+__LIVE_ROUTE="/live_stream/{user}"
+__REPO_ROUTE="/repo_ukory"
+reflex export --frontend-only #se añade la línea de variable de entorno para especificar la "api_url" al exportar el proyecto
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
+rm -f remote_build.sh
